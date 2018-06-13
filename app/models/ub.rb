85551324265b4949579237c26d0671a21f438676
Juliano <julianobsg@gmail.com>
2018-06-13 16:52:11 -0300

    Create ubs model

---
+++ app/models/ub.rb
@@ -0,0 +1,2 @@
+class Ub < ApplicationRecord
+end
---
+++ db/migrate/20180613194138_create_ubs.rb
@@ -0,0 +1,18 @@
+class CreateUbs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ubs do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :phone
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
+      t.integer :size
+      t.integer :adaptation_for_senior
+      t.integer :medical_equipment
+      t.integer :medicine
+
+      t.timestamps
+    end
+  end
+end
